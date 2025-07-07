3b4e7d809a2dfbaf14fb65294ff1fdeee99f18ff
Qczer <119138392+Qczer@users.noreply.github.com>
2025-07-07 18:30:44 +0200

    Trying deploy to Azure

--- EcommerceApi/obj/Debug/net9.0/EcommerceApi.AssemblyInfo.cs
+++ EcommerceApi/obj/Debug/net9.0/EcommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e55d93bac3a314fd69c925de3cf9d16e8d8c4dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a39e1679e6813ca848d6f6bfd834e48cfdf96b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
