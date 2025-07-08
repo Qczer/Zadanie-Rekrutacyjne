89141daa7a92d53027391815c25c9e85c9691ce0
Qczer <119138392+Qczer@users.noreply.github.com>
2025-07-08 10:50:09 +0200

    Fixed Product

--- EcommerceApi/Models/Product.cs
+++ EcommerceApi/Models/Product.cs
@@ -7,5 +7,7 @@ namespace EcommerceApi.Models
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public decimal Price { get; set; }
+        [JsonIgnore]
+        public List<OrderProduct> OrderProducts { get; set; } = new();
     }
 }
